d5a5cc2a101ffadfba1baed43b35292d48274582
Per Zetterlund <per.zetterlund@amraab.se>
2015-03-06 21:44:21 +0100

    Net class

--- sim74.py
+++ sim74.py
@@ -8,28 +8,44 @@ def bitsToInt(*bits):
       
    return val
 
+class Net(object):
+   def __init__(self, name):
+      self.terminals = []
+      self.name = name
+      
+   def getValue(self):
+      pass
+   
+   def __repr__(self):
+      pins = [str("%s:" % k) for k in self.terminals]
+      return "%s: %s" % (self.name, pins)
+   
+   def addPin(self, pin):
+      self.terminals.append(pin)
+      pin.addNet(self)
+
 class Pin(object):
    INPUT = 0
    OUTPUT = 1
    TRISTATE = 2
    
-   def __init__(self, direction):
+   def __init__(self, part, direction):
       self.direction = direction
-      self.connected = []
+      self.part = part
       self.value = 0
+      self.net = None
       
-   def addConnection(self, part):
-      self.connected.append(part)
-      
-   def __repr__(self):
-      connectedTo = [p.name for p in self.connected]
-      return '\n'.join(connectedTo)
-   
    def update(self):
       pass
    
    def getValue(self):
       return self.value
+   
+   def __repr__(self):
+      return "%s: %s" % (self.part.name, "pin xx")
+   
+   def setNet(self, net):
+      self.net = net
 
 class Part(object):
    def __init__(self, name):
@@ -42,15 +58,15 @@ class Part(object):
    def __repr__(self):
       pins = [str("%s: %s" % (k, repr(v))) for k,v in self.pins.items()]
       return "Part %s \n%s" % (self.name, '\n'.join(pins))
-      
+   
 class Header(Part):
    matchingNames = ["PINHD-1X4"]
    def __init__(self, name):
       Part.__init__(self, name)
-      self.pins = {'1': Pin(Pin.INPUT),
-                   '2': Pin(Pin.INPUT),
-                   '3': Pin(Pin.INPUT),
-                   '4': Pin(Pin.INPUT)}
+      self.pins = {'1': Pin(self, Pin.INPUT),
+                   '2': Pin(self, Pin.INPUT),
+                   '3': Pin(self, Pin.INPUT),
+                   '4': Pin(self, Pin.INPUT)}
 
 class Part7400(Part):
    def __init__(self, name):
@@ -61,28 +77,28 @@ class P74181(Part7400):
    
    def __init__(self, name):
       Part7400.__init__(self, name)
-      self.pins = {'A0': Pin(Pin.INPUT),
-                   'A1': Pin(Pin.INPUT),
-                   'A2': Pin(Pin.INPUT),
-                   'A3': Pin(Pin.INPUT),
-                   'B0': Pin(Pin.INPUT),
-                   'B1': Pin(Pin.INPUT),
-                   'B2': Pin(Pin.INPUT),
-                   'B3': Pin(Pin.INPUT),
-                   'F0': Pin(Pin.OUTPUT),
-                   'F1': Pin(Pin.OUTPUT),
-                   'F2': Pin(Pin.OUTPUT),
-                   'F3': Pin(Pin.OUTPUT),
-                   'S0': Pin(Pin.INPUT),
-                   'S1': Pin(Pin.INPUT),
-                   'S2': Pin(Pin.INPUT),
-                   'S3': Pin(Pin.INPUT),
-                   'CN': Pin(Pin.INPUT),
-                   'CN+4': Pin(Pin.OUTPUT),
-                   'M': Pin(Pin.INPUT),
-                   'A=B': Pin(Pin.OUTPUT),
-                   'G': Pin(Pin.OUTPUT),
-                   'P': Pin(Pin.OUTPUT)}
+      self.pins = {'A0': Pin(self, Pin.INPUT),
+                   'A1': Pin(self, Pin.INPUT),
+                   'A2': Pin(self, Pin.INPUT),
+                   'A3': Pin(self, Pin.INPUT),
+                   'B0': Pin(self, Pin.INPUT),
+                   'B1': Pin(self, Pin.INPUT),
+                   'B2': Pin(self, Pin.INPUT),
+                   'B3': Pin(self, Pin.INPUT),
+                   'F0': Pin(self, Pin.OUTPUT),
+                   'F1': Pin(self, Pin.OUTPUT),
+                   'F2': Pin(self, Pin.OUTPUT),
+                   'F3': Pin(self, Pin.OUTPUT),
+                   'S0': Pin(self, Pin.INPUT),
+                   'S1': Pin(self, Pin.INPUT),
+                   'S2': Pin(self, Pin.INPUT),
+                   'S3': Pin(self, Pin.INPUT),
+                   'CN': Pin(self, Pin.INPUT),
+                   'CN+4': Pin(self, Pin.OUTPUT),
+                   'M': Pin(self, Pin.INPUT),
+                   'A=B': Pin(self, Pin.OUTPUT),
+                   'G': Pin(self, Pin.OUTPUT),
+                   'P': Pin(self, Pin.OUTPUT)}
 
    def update(self):
       a = bitsToInt(*[pins[name].getValue() for name in ['A0', 'A1', 'A2', 'A3']])
@@ -108,24 +124,29 @@ def parseXml(filename):
    tree = ET.parse(filename)
    root = tree.getroot()
    parts = root.findall("./drawing/schematic/parts/part")
-   nets  = root.findall("./drawing/schematic/sheets/sheet/nets/net/segment")
+   netsNode  = root.findall("./drawing/schematic/sheets/sheet/nets")[0]
    
    pf = PartFactory()
 
    parts = {part.attrib['name']: pf.createPart(part.attrib['deviceset'], part.attrib['name']) for part in parts}
    
+   nets = []
+   
+   for netNode in netsNode:
+      
+      net = Net(netNode.attrib['name'])
+      nets.append(net)
+      
+      for segmentNode in netNode:
+         for pinrefOrWire in segmentNode:
+            if pinrefOrWire.tag == 'pinref':
+               net.addPin(parts[pinrefOrWire.attrib['part']].getPin(pinrefOrWire.attrib['pin']))
+         
    for net in nets:
-      pinrefs = net.findall('pinref')
-      connectedParts = [parts[pinref.attrib['part']] for pinref in pinrefs]
-      for pinref in pinrefs:
-         for part in connectedParts:
-            thisPart = parts[pinref.attrib['part']]
-            if thisPart != part:
-               thisPart.getPin(pinref.attrib['pin']).addConnection(part)
-               
-               
-   for part in parts.values():
-      print(part)
+      print(net)
+         
+   #for part in parts.values():
+   #   print(part)
    
 if __name__ == '__main__':
    parseXml('/home/per/eagle/test.sch')
