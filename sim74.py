923f05a1a8d532621c8cc01aa4d5b0e2fec7dd1c
Per Zetterlund <per.zetterlund@amraab.se>
2015-03-08 13:26:34 +0100

    System class

--- sim74.py
+++ sim74.py
@@ -1,49 +1,16 @@
 
-from parsexml import parseXml
-from toposort import toposort, toposort_flatten
+from system import System
 
-from core import Pin
+def testALU():
+   s = System('/home/per/eagle/test.sch')
+   
+   s.setInput('IN_A', 8)
+   s.setInput('IN_B', 8)
+   s.setInput('IN_S', 8)
+   s.setOutput('OUT_F', 8)
+   
+   s.run(3, 2, 0)
+   s.run(86, 47, 0)
    
 if __name__ == '__main__':
-   parts, nets = parseXml('/home/per/eagle/test.sch')
-
-   parts['IN_A'].setDirection(Pin.OUTPUT)
-   parts['IN_A'].setWidth(8)
-   parts['IN_B'].setDirection(Pin.OUTPUT)
-   parts['IN_B'].setWidth(8)
-   
-   parts['IN_S'].setDirection(Pin.OUTPUT)
-   parts['IN_S'].setNumber(0)
-   
-   parts['OUT_F'].setWidth(8)
-
-   parts['IN_A'].setNumber(3)
-   parts['IN_B'].setNumber(2)
-
-   totalDAGs = {}
-   
-   for p in parts.values():
-      totalDAGs.update(p.getDAGs())
-      
-   for n in nets:
-      totalDAGs.update(n.getDAG())
-
-   sorted = toposort_flatten(totalDAGs)
-   sorted.reverse()
-   
-   for s in sorted:
-      if s.direction == Pin.OUTPUT and s.net:
-         s.part.update()
-
-      
-   print(parts['OUT_F'].getNumber())
-   
-   parts['IN_A'].setNumber(86)
-   parts['IN_B'].setNumber(47)
-   
-   for s in sorted:
-      if s.direction == Pin.OUTPUT and s.net:
-         s.part.update()
-         
-         
-   print(parts['OUT_F'].getNumber())
+   testALU()
---
+++ system.py
@@ -0,0 +1,48 @@
+
+from parsexml import parseXml
+from toposort import toposort_flatten
+
+from core import Pin
+
+class System(object):
+   def __init__(self, filename):
+      self.parts, self.nets = parseXml(filename)
+      
+      self.inputs = []
+      self.outputs = []
+      
+   def setOutput(self, pinName, width):
+      self.parts[pinName].setDirection(Pin.INPUT)
+      self.parts[pinName].setWidth(width)
+      self.outputs.append(self.parts[pinName])
+      
+      
+   def setInput(self, pinName, width):
+      self.parts[pinName].setDirection(Pin.OUTPUT)
+      self.parts[pinName].setWidth(width)
+      self.inputs.append(self.parts[pinName])
+   
+   def run(self, *args):
+      assert len(args) == len(self.inputs)
+      
+      for i in range(len(args)):
+         self.inputs[i].setNumber(args[i])
+         
+      totalDAGs = {}
+      
+      for p in self.parts.values():
+         totalDAGs.update(p.getDAGs())
+         
+      for n in self.nets:
+         totalDAGs.update(n.getDAG())
+
+         
+      order = toposort_flatten(totalDAGs)
+      order.reverse()
+         
+      for s in order:
+         if s.direction == Pin.OUTPUT and s.net:
+            s.part.update()
+            
+      for o in self.outputs:
+         print("%s: %d" % (o.name, o.getNumber()))
